0642670d4989b8d8969944192148e4efce578020
mrflemmardmcpc-gif <mrflemmardmcpc-gif@users.noreply.github.com>
2026-01-25 07:56:39 +0100

    Remove Draft.js, markdownToDraft and dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
